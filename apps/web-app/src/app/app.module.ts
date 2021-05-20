93d81bf89235a5ce0a7b20e514321b70643694ce
Mohsen-MSK <mohsen77sk@gmail.com>
2021-05-20 21:41:04 +0430

    update libs and app

--- apps/web-app/src/app/app.module.ts
+++ apps/web-app/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { GetAppConfigProvider } from '@msk/shared/app-config';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [GetAppConfigProvider(environment)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  projects: ['<rootDir>/apps/web-app', '<rootDir>/apps/web-api'],
+  projects: [
+    '<rootDir>/apps/web-app',
+    '<rootDir>/apps/web-api',
+    '<rootDir>/libs/shell/feature',
+    '<rootDir>/libs/shell/ui/layout',
+    '<rootDir>/libs/shared/app-config',
+    '<rootDir>/libs/shared/utils',
+  ],
 };
