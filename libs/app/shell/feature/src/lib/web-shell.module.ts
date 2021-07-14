dde5189a0c52fe4f0fc0ba9450135c43a7f51cb0
m.sohrabi <mohsen77sk@gmail.com>
2021-07-14 16:49:49 +0430

    update router config

--- libs/app/shell/feature/src/lib/web-shell.module.ts
+++ libs/app/shell/feature/src/lib/web-shell.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
 
 import { webShellRoutes } from './web-shell.routes';
 
 import { LayoutModule } from '@msk/app/shell/ui/layout';
 import { SplashScreenModule } from '@msk/app/shell/ui/splash-screen';
 
+const routerConfig: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  preloadingStrategy: PreloadAllModules,
+};
+
 @NgModule({
   imports: [
     LayoutModule,
     SplashScreenModule,
 
-    RouterModule.forRoot(webShellRoutes, {
-      scrollPositionRestoration: 'top',
-    }),
+    RouterModule.forRoot(webShellRoutes, routerConfig),
   ],
   exports: [RouterModule],
 })
