a6d3b103f6089c21a746eb8bef402e95810dd7cf
m.sohrabi <mohsen77sk@gmail.com>
2022-01-22 16:35:21 +0330

    update libs and app

--- libs/client/shared/ui/alert/src/lib/alert.module.ts
+++ libs/client/shared/ui/alert/src/lib/alert.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MskAlertComponent } from './alert.component';
+
 @NgModule({
-  imports: [CommonModule],
+  declarations: [MskAlertComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  exports: [MskAlertComponent],
 })
-export class ClientSharedUiAlertModule {}
+export class MskAlertModule {}
