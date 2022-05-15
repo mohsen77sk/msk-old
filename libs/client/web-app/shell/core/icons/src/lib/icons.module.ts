1c39757d6c4a8a2090a749e8b177bad52cd50801
m.sohrabi <mohsen77sk@gmail.com>
2022-05-15 10:34:48 +0430

    update libs and app

--- libs/client/web-app/shell/core/icons/src/lib/icons.module.ts
+++ libs/client/web-app/shell/core/icons/src/lib/icons.module.ts
@@ -12,9 +12,10 @@ export class IconsCoreModule {
     private _matIconRegistry: MatIconRegistry
   ) {
     //
-    this._matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
-    //
+    this._matIconRegistry.setDefaultFontSetClass('material-symbols-rounded');
+    // 
     this._matIconRegistry.registerFontClassAlias('mat_solid', 'material-icons');
     this._matIconRegistry.registerFontClassAlias('mat_outline', 'material-icons-outlined');
+    this._matIconRegistry.registerFontClassAlias('mat_symbols_rounded', 'material-symbols-rounded');
   }
 }
