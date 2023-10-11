746eff3fd986b8b55573154d4a74a5202deee4f6
Mohsen77sk <Mohsen77sk@gmail.com>
2023-10-11 15:51:11 +0330

    update libs and app

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'msk-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  // Private
-  private _unsubscribeAll: Subject<any>;
-
+export class AppComponent implements OnInit {
   /**
    * Constructor
    */
-  constructor(@Inject(DOCUMENT) private document: any) {
-    // Set the private defaults
-    this._unsubscribeAll = new Subject();
-  }
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   // -----------------------------------------------------------------------------------------------------
   // @ Lifecycle hooks
@@ -30,17 +22,4 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     console.log('start');
   }
-
-  /**
-   * On destroy
-   */
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next(null);
-    this._unsubscribeAll.complete();
-  }
-
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
 }
